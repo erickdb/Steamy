cc21984647b4fb123769743c86468a78561ab4dd
Erick Darmawan <90923040+erickdb@users.noreply.github.com>
2026-04-04 14:17:23 +0700

    Update ll.lua

--- ll.lua
+++ ll.lua
@@ -32,7 +32,6 @@ local commonWords = {
 -- Dictionary URLs (fallback)
 local DICTIONARY_URLS = {
     "https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt"
-    "https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-usa-no-swears.txt"
 }
 
 -- GLOBAL VARIABLES ===========================================================
